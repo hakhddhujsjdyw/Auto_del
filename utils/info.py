c9aec7879b26ff1a85bcc02da265456a2847eeb3
hakhddhujsjdyw <manafalthaf01@gmail.com>
2024-11-23 11:29:51 +0530

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -18,12 +18,12 @@
 
 import os
 
-API_ID       = int(os.environ.get("API_ID", ""))
-API_HASH     = os.environ.get("API_HASH", "")
-BOT_TOKEN    = os.environ.get("BOT_TOKEN", "")
-SESSION      = os.environ.get("SESSION", "")
-TIME         = int(os.environ.get("TIME", 10))
-CHATS        = [int(cht) for cht in os.environ.get("CHATS", "").split()]
+API_ID       = int(os.environ.get("API_ID", "28704037"))
+API_HASH     = os.environ.get("API_HASH", "e27aa014fb9668024cb7375d90014493")
+BOT_TOKEN    = os.environ.get("BOT_TOKEN", "7911558932:AAFU9ksjmqWqlBiLgcZcp7c8H1Pa_mR_WZY")
+SESSION      = os.environ.get("SESSION", "BQEjAcAAje8tfsiqbkJTc2zycwtbqfrENn1EPRC3HfI_ax_Zr7hB2PnATvTF-AaHV9S0pLnx4hW8cBWhMMW7-IKtSR1m26gC8o0edAzhikphc1CMmo5TTIc3hbTY-SBE_xypQ-v_rXtPhG7DhdcYiJOikKk0j_em-I-1IoHsxg_VbaNRWZqrBKaMgAws6gI6VnmU1kcr4TBRQQ0uXjlsREu2Ie5E32NglBscUllxcySd2Rg_pyMQiDsJ9IVXCaFbLwBlqjVJilKoc5BOCkJTUmNhGcgs8mwLAV71NhoP4bby_VTf1I5lYu1OK-XBYP6gIoR1-n84o7HKzUo34RRSRRYQTHvXcwAAAAAgJabsAA")
+TIME         = int(os.environ.get("TIME", 7))
+CHATS        = [int(cht) for cht in os.environ.get("CHATS", "-1002413806904 -1002423467975 -1002398417852 -1002197221746 -1002416597824").split()]
 WHITE_LIST   = [int(wht) for wht in os.environ.get("WHITE_LIST", "").split()]
 BLACK_LIST   = [int(blk) for blk in os.environ.get("BLACK_LIST", "").split()]
 DATABASE_URI = os.environ.get("DATABASE_URI", "")
